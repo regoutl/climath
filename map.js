14d08796aaa8cdf678592f7c79cdffdd76c63045
Louis Regout <la.regout@gmail.com>
2020-01-03 00:45:06 +0100

    map header

---
+++ map.js
@@ -0,0 +1,30 @@
+class Map{
+	/// load the map data (land use, 
+	/// 
+	constructor(k){
+	}
+	
+	/// return the land use at a given pixel.
+	/// ans format : pop  => int,
+	///              solar => {//can be undefined
+	///				 },
+	///              nuke => { //can be undefined
+	///				 }
+	///				 bat => { //can be undefined
+	///              }
+	/// baseLandUse => { //undefined = out of country
+	///     City,    
+	///     Field
+	///     Forest
+	///     Water
+	///     economicalInterestArea
+	///     Airport
+	/// }
+	getPx(x, y){
+	}
+	
+	/// set pixel x, y with value with same format as get
+	setPx(x, y, landUse){
+	}
+	
+}
