770494de38b0938f49735137fc0edfa3c0676ffd
Louis Regout <la.regout@gmail.com>
2020-01-03 00:49:01 +0100

    tentative passage land use enum

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # benergy
 simulateur de transition energetique
+
+note d'installation :
+si certaines requetes ajax echouent, lancer via lamp
--- map.js
+++ map.js
@@ -1,3 +1,16 @@
+	const GroundUse = {
+		city: 4280237790,
+		forest: 4281259417,
+		forest2: 4279864386,
+		water: 4292140191,
+		field: 4288667881,
+		field2: 4283420126,
+		industry: 4290663815,
+		airport: 4290756057,
+		heath: 4290663815
+	}
+
+
 class Map{
 	/// load the map data (land use, 
 	/// 
